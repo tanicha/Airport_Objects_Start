dfd564aec151e38f08bf241a2235a3d4126b6e6f
tanicha <tanijcha@gmail.com>
2023-02-21 13:41:10 -0800

    created classes for 3 files

--- Airport.js
+++ Airport.js
@@ -1,11 +1,11 @@
 class Airport {
 	constructor(name) {
-		this.name = name
-		this.planes = //what would be the best datatype?
+		this.name = name,
+		this.planes = []
 	}
 
 	addPlane(plane) {
-		//use an array method
+		this.planes.push(plane)
 	}
 }
 
--- Bag.js
+++ Bag.js
@@ -2,7 +2,20 @@ class Bag {
     // Include constructor Method with the following property: 
         // 1. Weight
             //1a. Should handle an exception in case user adds no weight and throw an error.
-  
+        constructor (weight){
+            if (!weight){
+                throw new Error ('bag needs weight')
+            }
+            this.weight = weight;
+        }
+    }     
+
+    try {
+        const bag = new Bag (20)
+    } catch (err){
+        console.log(err)
 }
 
 //Export the module
+
+module.exports = Bag;
